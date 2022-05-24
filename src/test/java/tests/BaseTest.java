9495aee8d97d1ec75c0a4b0028ecb6a77a735c7f
Tautvydas Dulskis <tautvydas@dulskis.eu>
2022-05-24 23:01:37 +0300

    added test cases

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utilities.Driver;
+
+public class BaseTest {
+
+    @BeforeMethod
+    public void setup(){
+        Driver.setDriver();
+    }
+    @AfterMethod
+    public void teardrop(){
+        Driver.closeDriver();
+    }
+}
+
