5b1d6aa8e51924eca8ee30547e7a94bd1c75e678
Tautvydas Dulskis <tautvydas@dulskis.eu>
2022-05-25 10:19:52 +0300

    Add testNG.xml; modify driver

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -5,16 +5,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Driver {
-    private static WebDriver driver; //bus viena kopija sio obj
+    private static ThreadLocal<WebDriver>driver = new ThreadLocal<>();
+    //bus viena kopija sio obj
 
     public static void setDriver(){
         WebDriverManager.chromedriver().setup();
-         driver = new ChromeDriver();
+         driver.set (new ChromeDriver());
     }
     public static WebDriver getDriver(){
-        return driver;
+        return driver.get();
     }
     public static void closeDriver(){
-        driver.quit();
+        driver.get().quit();
+        driver.remove();
     }
-}
+
+  //  private static WebDriver driver; //bus viena kopija sio obj
+
+  //  public static void setDriver(){
+  //      WebDriverManager.chromedriver().setup();
+   //     driver = new ChromeDriver();
+  //  }
+  //  public static WebDriver getDriver(){
+   //     return driver;
+    }
+   // public static void closeDriver(){
+     //   driver.quit();
+   // }
